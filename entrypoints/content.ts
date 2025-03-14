0e140133c3fe468abafdb555c065b01648efb49a
Chang <AresChang114@qq.com>
2025-03-14 14:01:34 +0800

    chore: 添加 content Vue 组件渲染

--- entrypoints/content.ts
+++
@@ -1,8 +0,0 @@
-export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    setTimeout(() => {
-      console.log('Hello content.');
-    }, 1000);
-  },
-});
---
+++ entrypoints/content/index.ts
@@ -0,0 +1,26 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+export default defineContentScript({
+  matches: ['<all_urls>'],
+  cssInjectionMode: 'ui',
+
+  async main(ctx) {
+    const ui = await createShadowRootUi(ctx, {
+      name: 'quick-view-ui',
+      position: 'inline',
+      anchor: 'body',
+      onMount: (container) => {
+        const app = createApp(App);
+        app.mount(container);
+
+        return app;
+      },
+      onRemove: (app) => {
+        app?.unmount();
+      },
+    });
+
+    ui.mount();
+  },
+});
