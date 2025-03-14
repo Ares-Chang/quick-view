dd7bba9ca555776e0a47501ce7381795a44e0be8
Chang <AresChang114@qq.com>
2025-03-14 13:51:12 +0800

    chore: init 迁移到 wxt

--- src/components/Logo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <a class="mx-2 text-2xl icon-btn" rel="noreferrer" href="https://github.com/antfu/vitesse-webext" target="_blank" title="GitHub">
-    <i i-pixelarticons-power text-2xl icon-btn />
-  </a>
-</template>
