060bc117e3da36ae3a9dae0e178698fe3833ef24
Chang <AresChang114@qq.com>
2025-03-15 17:46:50 +0800

    chore: 引入 VueUse 及 unplugin-vue-components

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,5 @@
 import UnoCSS from 'unocss/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'wxt'
 
 // See https://wxt.dev/api/config.html
@@ -7,8 +8,19 @@ export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-vue'],
 
+  imports: {
+    presets: [
+      '@vueuse/core',
+    ],
+  },
+
   vite: () => ({
     plugins: [
+      // https://github.com/antfu/vite-plugin-components
+      Components({
+        dts: true,
+      }),
+
       // https://github.com/antfu/unocss
       // see uno.config.ts for config
       UnoCSS(),
