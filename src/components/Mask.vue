0105277195bfb2b5c3f8501a9a8f1888b81d5337
Chang <AresChang114@qq.com>
2025-03-17 17:02:11 +0800

    style: 背景模糊蒙层

---
+++ src/components/Mask.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    flex items-center justify-center
+    transition duration-400 ease-in-out
+    backdrop="blur-none has-[#mask-box]:hover:blur-sm"
+    pointer-events="auto has-[#mask-box]:hover:none"
+  >
+    <div id="mask-box" h-fit w-fit>
+      <slot />
+    </div>
+  </div>
+</template>
--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -3,19 +3,19 @@ const { url } = useData()
 </script>
 
 <template>
-  <div
-    fixed z-999 h-220 w-320
-    flex="~ col" gap-2
-    rd-xl p-2 font-sans
-    left="1/2" top="1/2"
-    text="center gray-700 dark:gray-200"
-    bg="gray-100 dark:gray-950"
-    transition duration-200 ease-in-out
-    border="~ gray-200 dark:gray-800"
-    class="translate--1/2"
-  >
-    <TooleBar select-none />
+  <Mask fixed left-0 top-0 z-999 h-screen w-screen>
+    <div
+      h-90vh w-95vw
+      flex="~ col" gap-2
+      rd-xl p-2 font-sans
+      text="center gray-700 dark:gray-200"
+      bg="gray-100 dark:gray-950"
+      transition duration-200 ease-in-out
+      border="~ gray-200 dark:gray-800"
+    >
+      <TooleBar select-none />
 
-    <iframe :src="url" w-full flex-1 rd-md />
-  </div>
+      <iframe :src="url" w-full flex-1 rd-md />
+    </div>
+  </Mask>
 </template>
