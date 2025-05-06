73b4b45d4753d9db13c1370df64e67bdab174bf1
Chang <AresChang114@qq.com>
2025-05-06 11:34:58 +0800

    chore: update wxt version package

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'wxt'
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  extensionApi: 'chrome',
   modules: ['@wxt-dev/module-vue'],
 
   manifest: {
